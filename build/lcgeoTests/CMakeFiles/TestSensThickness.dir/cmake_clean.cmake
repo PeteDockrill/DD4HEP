4f15411e1c439ec788946fae9d4bdc5f63c7f2ee
Peter Dockrill [JG 1811] <zcempjd@ucl.ac.uk>
2019-01-07 20:34:47 +0000

    After example Sim before reco

---
+++ build/lcgeoTests/CMakeFiles/TestSensThickness.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestSensThickness.dir/src/TestSensThickness.cpp.o"
+  "../bin/TestSensThickness.pdb"
+  "../bin/TestSensThickness"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestSensThickness.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
